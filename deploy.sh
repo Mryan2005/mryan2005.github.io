7dc20b07e235a91bb65ab3a8a4ba71fac76e0d87
Mryan2005 <40586009+Mryan2005@users.noreply.github.com>
2020-07-28 21:01:00 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ rm -rf .git
 git init
 git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"  # 提交记录包含时间 跟上面更改时区配合
-git push --force --quiet "https://Mryan05:${Travis_Token}@e.coding.net:Mryan05/mryan05.coding.me.git" master:master
+git push --force --quiet "https://Mryan05:${Travis_Token}@e.coding.net/Mryan05/mryan05.coding.me.git" master:master
