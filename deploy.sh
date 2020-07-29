607f52e4bac6b638ec5da8e53b249b242261bae6
Mryan2005 <40586009+Mryan2005@users.noreply.github.com>
2020-07-29 16:54:03 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-git config --global user.name "Mryan05"
-git config --global user.email "a2564011261@163.com"
+rm -rf ./public/.git
+cd ..
+git clone https://e.coding.net/Mryan05/mryan05.coding.me.git coding
+mv ./coding/.git/ ./public/
 cd ./public
-rm -rf .git
-git init
+git config --global user.name "Mryan05"
+git config --global user.email "A2564011261@163.com"
 git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"  # 提交记录包含时间 跟上面更改时区配合
 git push --force --quiet "https://Mryan05:${Travis_Token}@e.coding.net/Mryan05/mryan05.coding.me.git" master:master
