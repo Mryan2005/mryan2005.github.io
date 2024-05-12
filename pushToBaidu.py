a45247f65cef557608ee0484ee4bd8019fa02b68
Mryan2005 <Mryan2005@users.noreply.github.com>
2024-05-12 08:30:25 +0000

    deploy: 2d8e1f8eabcade7b41d745685320c263ff93c2a8

--- pushToBaidu.py
+++ pushToBaidu.py
@@ -3,16 +3,16 @@ import os
 import sys
 
 def createUrlTxt(url):
-    with open('url.txt', 'w') as f:
+    with open('urls.txt', 'w') as f:
         for root, dirs, files in os.walk("."):
             for file in files:
                 if file.endswith('.html'):
-                    f.write(url + '/' + os.path.join(root, file)[7:] + '\n')
+                    f.write(url + '/' + os.path.join(root, file)[:] + '\n')
 
 if __name__ == '__main__':
     createUrlTxt(sys.argv[1])
-    print('url.txt created')
-    with open('url.txt') as f:
+    print('urls.txt created')
+    with open('urls.txt') as f:
         urls = f.readlines()
     for url in urls:
         print(url)
