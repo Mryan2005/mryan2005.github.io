05b13a1036ce1cfa73c66bd59bd389b0dec0238d
Mryan2005 <Mryan2005@users.noreply.github.com>
2024-05-22 04:46:08 +0000

    deploy: 31631af1356b3473a8c6e2ba88c24e5f2d03788e

--- pushToBaidu.py
+++ pushToBaidu.py
@@ -18,8 +18,11 @@ def createUrlTxt(url):
 if __name__ == '__main__':
     links = createUrlTxt(sys.argv[1])
     siteurl = sys.argv[1]
-    baiduToken = sys.argv[2]
-    bingToken = sys.argv[3]
+    if "indexNew" in sys.argv:
+        bingIndexNewToken = sys.argv[2]
+    else:
+        baiduToken = sys.argv[2]
+        bingToken = sys.argv[3]
     needToPush = []
     # random push 10 urls
     for i in range(0, 10):
@@ -34,28 +37,41 @@ if __name__ == '__main__':
                 break
     print("we will push the url as follow", needToPush)
     # push to bing
-    headers = {
-        'Content-Type': 'application/json; charset=utf-8',
-        "Host": "ssl.bing.com"
-    }
-    data = {
-        "siteUrl": siteurl,
-        "urlList": needToPush
-    }
-    response = requests.post('https://www.bing.com/webmaster/api.svc/json/SubmitUrlbatch?apikey=' + bingToken, data=json.dumps(data), headers=headers)
+    if "indexNew" not in sys.argv:
+        headers = {
+            'Content-Type': 'application/json; charset=utf-8',
+            "Host": "ssl.bing.com"
+        }
+        data = {
+            "siteUrl": siteurl,
+            "urlList": needToPush
+        }
+        response = requests.post('https://www.bing.com/webmaster/api.svc/json/SubmitUrlbatch?apikey=' + bingToken, data=json.dumps(data), headers=headers)
+    elif "indexNew" in sys.argv:
+        headers = {
+            'Content-Type': 'application/json; charset=utf-8',
+            "Host": "api.indexnow.org"
+        }
+        data = {
+            "host": siteurl.split('://')[1],
+            "key": bingIndexNewToken,
+            "keyLocation": siteurl + bingIndexNewToken + '.txt',
+            "urlList": needToPush
+        }
+        response = requests.post('https://www.bing.com/IndexNow', data=json.dumps(data), headers=headers)
     print(response.text)
-
-    # push to baidu
-    """
-    User-Agent: curl/7.12.1
-    Host: data.zz.baidu.com
-    Content-Type: text/plain
-    """
-    headers = {
-        'Content-Type': 'text/plain',
-        "Host": "data.zz.baidu.com",
-        "User-Agent": "curl/7.12.1"
-    }
-    data = '\n'.join(needToPush)
-    response = requests.post('http://data.zz.baidu.com/urls?site=' + siteurl + '&token=' + baiduToken, data=data, headers=headers)
-    print(response.text)
+    if "indexNew" not in sys.argv:
+        # push to baidu
+        """
+        User-Agent: curl/7.12.1
+        Host: data.zz.baidu.com
+        Content-Type: text/plain
+        """
+        headers = {
+            'Content-Type': 'text/plain',
+            "Host": "data.zz.baidu.com",
+            "User-Agent": "curl/7.12.1"
+        }
+        data = '\n'.join(needToPush)
+        response = requests.post('http://data.zz.baidu.com/urls?site=' + siteurl + '&token=' + baiduToken, data=data, headers=headers)
+        print(response.text)
