45571a62be5680221f085f5537782acf56591e01
Mryan2005 <Mryan2005@users.noreply.github.com>
2024-06-01 13:53:08 +0000

    deploy: 354e662fa7a5d01f429a776cf6e28f7dbb508f7e

--- js/plugins/tags/contributors.js
+++ js/plugins/tags/contributors.js
@@ -1,1 +1,92 @@
-const ContributorsJS={requestAPI:(o,s,l)=>{let i=5;!function n(){new Promise((r,t)=>{let e=0,a=setTimeout(()=>{0===e&&(e=2,a=null,t("请求超时"),0==i)&&l()},5e3);fetch(o).then(function(t){if(2!==e&&(clearTimeout(a),r(t),a=null,e=1),t.ok)return t.json();throw new Error("Network response was not ok.")}).then(function(t){i=0,s(t)}).catch(function(t){0<i?(--i,setTimeout(()=>{n()},5e3)):l()})})}()},layout:n=>{const r=n.el;ContributorsJS.requestAPI(n.api,function(t){r.querySelector(".loading-wrap").remove();var a="";(t||[]).forEach((t,r)=>{var e=(e=(e=(e='<div class="user-card">')+'<a class="card-link" target="_blank" rel="external noopener noreferrer"'+(' href="'+t.html_url+'">'))+('<img alt="'+t.login+'" src="'+(t.avatar_url||n.avatar)+'" onerror="errorImgAvatar(this)">'))+('<div class="name"><span>'+t.login+"</span></div>");a+=e=e+"</a>"+"</div>"}),r.querySelector(".group-body").innerHTML=a},function(){try{r.querySelector(".loading-wrap svg").remove(),r.querySelector(".loading-wrap p").innerText("加载失败，请稍后重试。")}catch(t){}})},start:()=>{for(var t=document.getElementsByClassName("contributorsjs-wrap"),r=0;r<t.length;r++){var e,a=t[r],n=a.getAttribute("api");null!=n&&((e=new Object).el=a,e.api=n,e.class=a.getAttribute("class"),e.avatar=volantis.GLOBAL_CONFIG.default.avatar,ContributorsJS.layout(e))}}};ContributorsJS.start(),document.addEventListener("pjax:complete",function(){ContributorsJS.start()});
+const ContributorsJS = {
+  requestAPI: (url, callback, timeout) => {
+    let retryTimes = 5;
+
+    function request() {
+      return new Promise((resolve, reject) => {
+        let status = 0; // 0 等待 1 完成 2 超时
+        let timer = setTimeout(() => {
+          if (status === 0) {
+            status = 2;
+            timer = null;
+            reject('请求超时');
+            if (retryTimes == 0) {
+              timeout();
+            }
+          }
+        }, 5000);
+        fetch(url).then(function (response) {
+          if (status !== 2) {
+            clearTimeout(timer);
+            resolve(response);
+            timer = null;
+            status = 1;
+          }
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error('Network response was not ok.');
+        }).then(function (data) {
+          retryTimes = 0;
+          callback(data);
+        }).catch(function (error) {
+          if (retryTimes > 0) {
+            retryTimes -= 1;
+            setTimeout(() => {
+              request();
+            }, 5000);
+          } else {
+            timeout();
+          }
+        });
+      });
+    }
+    request();
+  },
+  layout: (cfg) => {
+    const el = cfg.el;
+    ContributorsJS.requestAPI(cfg.api, function (data) {
+      el.querySelector('.loading-wrap').remove();
+      var cellALL = "";
+      (data || []).forEach((item, i) => {
+        var user = '<div class="user-card">';
+        user += '<a class="card-link" target="_blank" rel="external noopener noreferrer"';
+        user += ' href="' + item.html_url + '">';
+        user += '<img alt="' + item.login + '" src="' + (item.avatar_url || cfg.avatar) + '" onerror="errorImgAvatar(this)">';
+        user += '<div class="name"><span>' + item.login + '</span></div>';
+        user += '</a>';
+        user += '</div>';
+        cellALL += user;
+      });
+      el.querySelector('.group-body').innerHTML = cellALL;
+    }, function () {
+      try {
+        el.querySelector('.loading-wrap svg').remove();
+        el.querySelector('.loading-wrap p').innerText('加载失败，请稍后重试。');
+      } catch (e) { }
+    });
+  },
+  start: () => {
+    const els = document.getElementsByClassName('contributorsjs-wrap');
+    for (var i = 0; i < els.length; i++) {
+      const el = els[i];
+      const api = el.getAttribute('api');
+      if (api == null) {
+        continue;
+      }
+      var cfg = new Object();
+      cfg.el = el;
+      cfg.api = api;
+      cfg.class = el.getAttribute('class');
+      cfg.avatar = volantis.GLOBAL_CONFIG.default.avatar;
+      ContributorsJS.layout(cfg);
+    }
+  }
+}
+
+
+
+ContributorsJS.start();
+document.addEventListener('pjax:complete', function () {
+  ContributorsJS.start();
+});
